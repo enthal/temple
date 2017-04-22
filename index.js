a59d0359c8d90d26bda58c2539c1d0d50e7297b6
Tim James <tjames@tjames.com>
2017-04-21 23:42:29 -0700

    Basic Templating

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 #!/usr/bin/env node
+const _ = require('lodash')
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const glob = require('glob');
+const Promise = require('bluebird');
+
+Promise.promisifyAll(fs);
+const globAsync = Promise.promisify(glob);
+
 
 const main = () => {
-  log("hi");
+  const content = yaml.safeLoad(fs.readFileSync('./example/content.yml', 'utf8'));
+  log(content)
+
+  const templateDir = './example/templates/'
+  globAsync(templateDir+'/**/*')
+    .then((filenames)=>{
+      // log(filenames);
+      const templatesByName = _.fromPairs(_.map(filenames,(f)=>{
+        const name = /.*\/([^.]+).*/.exec(f)[1]
+        return [name,_.template(fs.readFileSync(f, 'utf8'))];
+      }));
+      _.each(content, (o)=>{
+        log(o);
+        log(templatesByName[o['$t']](o));
+      })
+    });
 }
 
 const log = console.log;
