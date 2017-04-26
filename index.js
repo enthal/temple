81d2941790c51aaa762678a0aef848c9101bb327
Tim James <tjames@tjames.com>
2017-04-26 13:48:54 -0700

    copy any static assets; use better libs

--- index.js
+++ index.js
@@ -1,73 +1,88 @@
 #!/usr/bin/env node
+const fs   = require('fs-extra');
 const _ = require('lodash')
 const yaml = require('js-yaml');
-const fs   = require('fs');
-const glob = require('glob');
+const glob = require('glob-all');
 const Promise = require('bluebird');
-const mkdirp = require('mkdirp');
 const marked = require('marked');
 const program = require('commander');
 
 Promise.promisifyAll(fs);
-const globAsync = Promise.promisify(glob);
-
 
 const main = () => {
   program
-    .option('-c, --content <path>', 'YAML/JSON content file')
-    .option('-t, --templates <path>', 'directory (recursively) containing template files')
-    .option('-o, --out <path>', 'output directory')
+    .option('-c, --content <path>',   '(REQUIRED) YAML/JSON content file')
+    .option('-t, --templates <path>', '(REQUIRED) directory (recursively) containing template files')
+    .option('-o, --out <path>',       '(REQUIRED) output directory')
+    .option('-s, --static <path>',    '(optional) directory (recursively) containing static files to copy to output directory')
     .parse(process.argv);
 
   _.each(['content','templates','out'], (x) => {
     if (!program[x]) {
-      log(`Option --${x} required.`);
+      log(`ERROR: Option required:  --${x}`);
       program.help();  // exits
     }
   });
 
-  const content = yaml.safeLoad(fs.readFileSync(program.content, 'utf8'));
+  const content = yaml.safeLoad(fs.readFileSync(program.content, 'utf8'));  // TODO: async
   log(content)
 
-  mkdirp.sync('out');
-  globAsync(program.templates + '/**/*')
-    .then((filenames)=>{
+  fs.emptyDirSync(program.out);
 
-      const templatesByName = _.fromPairs(_.map(filenames, (f) => {
-        const name = /.*\/([^.]+).*/.exec(f)[1]
-        return [name,_.template(fs.readFileSync(f, 'utf8'))];
-      }));
+  if (program.static) {
+    fs.copySync(program.static, program.out);
+  }
 
-      const recurse = (os) => {
-        if (os == null)     { os = []; }
-        if (!_.isArray(os)) { os = [os]; }
-        log(os)
-        return _.map(os, (o) => {
-          let text;
-          if (_.isPlainObject(o)) {
-            if (!o.$t) { throw new Error("Content object lacks $t:",o); }  // TODO: show path into content
-            text = templatesByName[o.$t](_.assign({
-                $:{ recurse:recurse },
-                _:_,
-              }, o), {sourceURL:o.$t});
-            if (o.$path) { fs.writeFileSync(program.out + o.$path, text); }
-          } else
-          if (_.isArray(o)) {
-            text = recurse(o);  // flatten nested arrays; TODO really?
-          } else {
-            o = (''+o);
-            text = marked(o);
-            if (! ~o.indexOf('\n')) {
-              const m = /^<p>(.*)<\/p>\s*$/.exec(text);
-              if (m) text = `<span>${m[1]}</span>`;
-              log(m,text)
-            }
-          }
-          return text;
-        }).join('\n');
+  const templateFilenames = glob.sync(program.templates + '/**/*');
+  log(templateFilenames);
+  const templatesByName = _.fromPairs(_.map(templateFilenames, (f) => {
+    const name = /.*\/([^.]+).*/.exec(f)[1];  // TODO: nest template naming (flattening here)
+    return [name,_.template(fs.readFileSync(f, 'utf8'))];  // TODO: async
+  }));
+
+  const recurse = (os) => {
+    if (os == null)     { os = []; }
+    if (!_.isArray(os)) { os = [os]; }
+    log(os)
+
+    return _.map(os, (o) => {
+      let text;
+
+      if (_.isPlainObject(o)) {
+        if (!o.$t) { throw new Error("Content object lacks $t"); }  // TODO: show path into content
+        const template = templatesByName[o.$t];
+        if (!template) { throw new Error("No template for $t: "+o.$t); }  // TODO: show path into content
+
+        text = template(_.assign({
+            $:{ recurse:recurse },
+            _:_,
+          }, o), {sourceURL:o.$t});
+
+        if (o.$path) {
+          fs.outputFileSync(program.out + o.$path, text);
+        }
+      } else
+
+      if (_.isArray(o)) {
+        text = recurse(o);  // flatten nested arrays; TODO really?
       }
-      _.each(content, recurse);
-    });
+
+      else {
+        o = (''+o);
+        text = marked(o);
+        if (! ~o.indexOf('\n')) {
+          const m = /^<p>(.*)<\/p>\s*$/.exec(text);
+          if (m) text = `<span>${m[1]}</span>`;
+          log(m,text)
+        }
+      }
+
+      return text;
+
+    }).join('\n');
+  }
+
+  _.each(content, recurse);
 }
 
 const log = console.log;
